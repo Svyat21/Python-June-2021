5b9f2d06598ca78e17b12f428ab3d52406d1f76c
sursv <sursvyat@gmail.com>
2021-06-30 16:44:48 +0300

    added serializers

--- applications/twits/serializers.py
+++ applications/twits/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-
 from applications.twits.models import *
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     full_name = serializers.SerializerMethodField()
@@ -23,8 +23,35 @@ class OrderSerializer(serializers.ModelSerializer):
 
 
 class MessageSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Message
         exclude = ()
 
+
+class PersonSerializer(serializers.ModelSerializer):
+    following = serializers.SerializerMethodField(method_name='get_following')
+    followers = serializers.SerializerMethodField(method_name='get_followers')
+
+    @staticmethod
+    def get_following(obj):
+        return [i.pk for i in obj.following.all()]
+
+    @staticmethod
+    def get_followers(obj):
+        return [i.pk for i in obj.followers.all()]
+
+    class Meta:
+        model = Person
+        exclude = ()
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'text', 'date_creation', 'author']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        exclude = ()
