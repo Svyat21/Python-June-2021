6d4a1c22467c8dec7e36606fc210d730c1771f0b
sursv <sursvyat@gmail.com>
2021-07-01 10:41:42 +0300

    edit messages

--- applications/twits/api/messages.py
+++ applications/twits/api/messages.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
 
 from applications.twits.models import Post
-from applications.twits.serializers import MessageSerializer
+from applications.twits.serializers import PostSerializer
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     model = Post
-    serializer_class = MessageSerializer
+    serializer_class = PostSerializer
 
     def get_queryset(self):
         return Post.objects.all()
